791417cc896bf40c4ce03791ddb0e6296f0eb7a1
Riccardo <riccardo.taranto@icloud.com>
2026-03-24 09:51:43 +0100

    pseudo code for pari e dispari

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('js-paliedispari');
 
-// ** Palidroma **
+// ** Palidroma ** ✅
 
 // Chiedere all’utente di inserire una parola
 const word = prompt('Inserire una parola');
@@ -17,13 +17,12 @@ if (isPalindroma(word)) {
     console.log(`${word} non è una parola palindroma`);
 }
 
+// ** Pari e Dispari **
 
+// Chiedere all'utente la sua scelta (pari/dispari) e il suo numero
 
-/* Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-Sommiamo i due numeri
-Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-Dichiariamo chi ha vinto.
-*/
+// Generare un numero random per il computer
 
+// Sommare i due numeri e capire se è pari o dispari
+
+// Dichiarare il vincitore
